cbf201e9537442cfd35e3889270c3b24ba6a3b0f
gaohailang <gaohailang@wandoujia.com>
2014-04-28 00:51:03 +0800

    feat(ux): update listView to support time-based add animation(one by one using ng-Fx)

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,37 +1,59 @@
-var app = angular.module('jvViewApp', ['ngAnimate', 'fx.animations', 'infinite-scroll']);
+var app = angular.module('jvViewApp', ['ngAnimate', 'fx.animations', 'infinite-scroll', 'angularSpinkit']);
 
-angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 250)
+angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 2500)
 
 app.controller('mainCtrl', function($scope, $timeout, ListViewAPI) {
     $scope.listView = new ListViewAPI();
 
-    $scope.$watch('listView', function(val) {
+    $scope.$watch('listView.newItems', function(val) {
+        if (!val) return;
+        _.each(val, function(item, idx) {
+            $timeout(function() {
+                $scope.listView.items.push(item);
+            }, idx * 300);
+        });
+        $timeout(function() {
+            $scope.listView.finishUX = true;
+        }, val.length * 300);
+    }, true);
+
+    $scope.$watch('listView.finishUX', function(val) {
         if (!val) return;
         $('#gallery').photobox('a', {
             time: 0
         });
-    }, true);
+    });
 });
 
-app.factory('ListViewAPI', function($http) {
+app.factory('ListViewAPI', function($http, $timeout) {
     var ListView = function() {
+        this.newItems = [];
         this.items = [];
         this.busy = false;
         this.links;
+        this.timer;
+        this.finishUX = false;
     };
 
     ListView.prototype.nextPage = function() {
         if (this.busy) return;
         this.busy = true;
+        this.timer = (new Date()).getTime();
 
         var url = "/api/v1/most_wanted";
         if (this.links && this.links.next) {
             url = this.links.next.href;
         }
         $http.get(url).then(function(resp) {
-            this.items = this.items.concat(resp.data._items);
+            this.newItems = resp.data._items;
             this.links = resp.data._links;
-            this.busy = false;
+            if ((new Date()).getTime() - (this.timer + 3000)) {
+                this.busy = false;
+            } else {
+                $timeout(function() {
+                    this.busy = false;
+                }, (this.timer + 3000) - (new Date()).getTime());
+            }
         }.bind(this));
     };
 
